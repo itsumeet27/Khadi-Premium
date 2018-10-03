1a656fde13dfbd9e44f346c8c97044ff04bb71ab
Sumeet Sharma <sksksharma0@gmail.com>
2018-10-03 18:07:36 +0530

    Add files via upload

---
+++ core/init.php
@@ -0,0 +1,13 @@
+<?php
+
+$db = mysqli_connect('localhost', 'root', '', 'khadipremium');
+if(mysqli_connect_errno()){
+	echo "Database connection failed with following errors: ".mysqli_connect_error();
+	die();
+
+}
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/khadi/config.php';
+require_once BASEURL.'helpers/helpers.php';
+
+?>
