759757a00de6f28b4312394c9ac57cb503db822c
Sumeet Sharma <sksksharma0@gmail.com>
2018-10-04 15:36:49 +0530

    Add files via upload

--- admin/products.php
+++ admin/products.php
@@ -1,9 +1,69 @@
 <?php
 	require_once $_SERVER['DOCUMENT_ROOT'].'/khadi/core/init.php';	
 	include 'includes/header.php';
-
 	if(isset($_GET['add'])){
 		$parentQuery = $db->query("SELECT * FROM categories WHERE parent = 0 ORDER BY category");
+		if($_POST){
+			$title = sanitize($_POST['title']);
+			$categories = sanitize($_POST['child']);
+			$dbpath = '';
+			$price = sanitize($_POST['price']);
+			$list_price = sanitize($_POST['list_price']);
+			$sku = sanitize($_POST['sku']);
+			$weight = sanitize($_POST['weight']);
+			$short_desc = sanitize($_POST['short_desc']);
+			$long_desc = sanitize($_POST['long_desc']);
+			$tagline = sanitize($_POST['tagline']);
+			$stock = sanitize($_POST['stock']);
+			$errors = array();
+			$required = array('title', 'price', 'parent', 'child', 'short_desc', 'long_desc', 'sku', 'weight', 'tagline');
+			foreach ($required as $field) {
+				if($_POST[$field] == ''){
+					$errors[] = 'All fields with * are required';
+					break;
+				}
+			}
+
+			if(!empty($_FILES)){
+				$photo = $_FILES['photo'];
+				$name = $photo['name'];
+				$nameArray = explode('.',$name);
+				$fileName = $nameArray[0];
+				$fileExt = $nameArray[1];
+				$mime = explode('/',$photo['type']);
+				$mimeType = $mime[0];
+				$mimeExt = $mime[1];
+				$tmpLoc = $photo['tmp_name'];
+				$fileSize = $photo['size'];
+				$allowed = array('png','jpg','jpeg','gif');
+				$uploadName = md5(microtime()).'.'.$fileExt;
+				$uploadPath = BASEURL.'images/products/'.$uploadName;
+				$dbpath = '/khadi/images/products/'.$uploadName;
+				if($mimeType != 'image'){
+					$errors[] = 'The file must be an image';
+				}
+				if(!in_array($fileExt, $allowed)){
+					$errors[] = 'The file extension must be a png, jpg, jpeg, or gif';
+				}
+				if($fileSize > 15000000){
+					$errors[] = 'The files size must be under 15 MB';
+				}
+				if($fileExt != $mimeExt && ($mimeExt == 'jpeg' && $fileExt != 'jpg')){
+					$errors[] = 'File extension does not match the file';
+				}
+			}
+
+			if(!empty($errors)){
+				echo display_errors($errors);
+			}else{
+				//Upload file and insert into database
+				move_uploaded_file($tmpLoc, $uploadPath);
+				$insertSql = "INSERT INTO products (`title`,`sku`,`price`,`list_price`,`categories`,`image`,`tagline`,`short_desc`,`long_desc`,`weight`,`stock`) VALUES ('$title','$sku','$price','$list_price','$categories','$dbpath','$tagline','$short_desc','$long_desc','$weight','$stock')";
+				$db->query($insertSql);
+			}
+		}
+
+
 		?>
 		<div class="container">
 		<h2 class="text-center">Add a new Product</h2>
@@ -63,13 +123,16 @@
 					<td><textarea id="tagline" name="tagline" class="form-control" rows="1"><?=((isset($_POST['tagline']))?sanitize($_POST['tagline']):'');?></textarea></td>
 				</tr>	
 				<tr>
-					<td><input type="submit" name="submit_product" value="Add Product" class="btn btn-success form-control"></td>
+					<th>Stock*</th>
+					<td><textarea id="stock" name="stock" class="form-control" rows="1"><?=((isset($_POST['stock']))?sanitize($_POST['stock']):'');?></textarea></td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="add" value="Add Product" class="btn btn-success form-control"></td>
 				</tr>				
 			</table>
 		</form>
 	</div>
 	<?php } 
-
 	else{
 		$sql = "SELECT * FROM products WHERE deleted = 0";
 		$presults = $db->query($sql);
@@ -104,7 +167,6 @@
 				$presult = $db->query($pSql);
 				$parent = mysqli_fetch_assoc($presult);
 				$category = $parent['category'].' - '.$child['category'];
-
 			?>
 				<tr>
 					<td>
