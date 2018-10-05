638fec7486cfae8c7538cd3344d2e499a47e7b7f
Sumeet Sharma <sksksharma0@gmail.com>
2018-10-05 17:31:06 +0530

    Add files via upload

--- core/init.php
+++ core/init.php
@@ -10,4 +10,23 @@ if(mysqli_connect_errno()){
 require_once $_SERVER['DOCUMENT_ROOT'].'/khadi/config.php';
 require_once BASEURL.'helpers/helpers.php';
 
+if(isset($_SESSION['Admin_User'])){
+	$user_id = $_SESSION['Admin_User'];
+	$query = $db->query("SELECT * FROM users WHERE id = '$user_id'");
+	$user_data = mysqli_fetch_assoc($query);
+	$fn = explode(' ', $user_data['full_name']);
+	$user_data['first'] = $fn[0];
+	$user_data['last'] = $fn[1];
+}
+
+if(isset($_SESSION['success_flash'])){
+	echo '<div class="bg-success"><p class="text-success text-center">'.$_SESSION['success_flash'].'</p></div>';
+	unset($_SESSION['success_flash']);
+}
+
+if(isset($_SESSION['error_flash'])){
+	echo '<div class = "bg-danger"><p class="text-danger text-center">'.$_SESSION['error_flash'].'</p></div>';
+	unset($_SESSION['error_flash']);
+}  
+
 ?>
