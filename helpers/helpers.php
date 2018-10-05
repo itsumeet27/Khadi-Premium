718a406cf483f02a0dd6b2aab87748bae7253b15
Sumeet Sharma <sksksharma0@gmail.com>
2018-10-05 17:30:49 +0530

    Add files via upload

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -19,4 +19,24 @@
 		return '&#8377; '.number_format($number);
 	}
 
+	function login($user_id){
+		$_SESSION['Admin_User'] = $user_id;
+		global $db;
+		$date = date("Y-m-d H:i:s");
+		$db->query("UPDATE users SET last_login = '$date' WHERE id = '$user_id'");
+		$_SESSION['success_flash'] = 'You have logged in successfully';
+	}
+
+	function is_logged_in(){
+		if(isset($_SESSION['Admin_User']) && $_SESSION['Admin_User'] > 0){
+			return true;
+		}
+		return false;
+	}
+
+	function login_error_redirect($url = 'login.php'){
+		$_SESSION['error_flash'] = 'Please login to access the page';
+
+	}
+
 ?>
