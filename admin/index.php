71126d7191feaf5186595aeadc70830e3baae343
Sumeet Sharma <sksksharma0@gmail.com>
2018-10-05 17:32:55 +0530

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,14 @@
 <?php
 
-	
-	include 'includes/header.php';
-	
-
+	require_once '../core/init.php';
+	if(!is_logged_in()){
+		login_error_redirect();
+	}
+	include 'includes/header.php'; 
 ?>
 
 <div class="container-fluid">
-	
+	Admin Home
 </div>
 
 <?php
