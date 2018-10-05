92668c800d5f36c65061c29f884689b3cd3d8eb5
Sumeet Sharma <sksksharma0@gmail.com>
2018-10-05 22:00:14 +0530

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,19 @@
-	
+	<script type="text/javascript">
+		function detailsmodal(id){
+			var data = {"id" : id};
+			jQuery.ajax({
+				url : '/khadi/includes/modal.php',
+				method : "post",
+				data : data,
+				success: function(data){
+					jQuery('body').append(data);
+					jQuery('#details-modal').modal('toggle');
+				},
+				error: function(){
+					alert("Something went wrong!");
+				}
+	 		})
+		}
+	</script>
 </body>
 </html>
