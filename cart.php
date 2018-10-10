6a423dbbe9386c46951f55f25a15a4efb5ca67f6
Sumeet Sharma <sksksharma0@gmail.com>
2018-10-10 15:14:25 +0530

    Add files via upload

--- cart.php
+++ cart.php
@@ -91,19 +91,135 @@
 
 				<!-- Modal -->
 				<div class="modal fade" id="checkoutModal" tabindex="-1" role="dialog" aria-labelledby="checkoutModalLabel">
-				  	<div class="modal-dialog" role="document">
+				  	<div class="modal-dialog modal-lg" role="document">
 				    	<div class="modal-content">
-				      	<div class="modal-header">
-				        	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				        	<h4 class="modal-title" id="checkoutModalLabel">Shipping Address</h4>
-				      	</div>
-				      	<div class="modal-body">
-				        ...
-				      	</div>
-				      	<div class="modal-footer">
-				        	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				        	<button type="button" class="btn btn-primary">Save changes</button>
-				      	</div>
+					      	<div class="modal-header">
+					        	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					        	<h4 class="modal-title text-center" id="checkoutModalLabel">Shipping Address</h4>
+					      	</div>
+					      	<div class="modal-body">
+					        	<div class="row">
+					        		<form action="thankYou.php" method="post" id="payment-form">
+					        			<span class="bg-danger text-danger" id="payment-errors"></span>
+						        		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						        			<img src="images/ban2.jpg" class="img-responsive">
+						        		</div>
+						        		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="step1" style="display: block;">       			
+					        				<table class="table table-responsive">			        					
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="full_name">Full Name</label></td>
+							        					<td><input type="text" name="full_name" id="full_name" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="email">Email</label></td>
+							        					<td><input type="email" name="email" id="email" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="address">House No./Block No.</label></td>
+							        					<td><input type="text" name="address" id="address" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="street_name">Street Name</label></td>
+							        					<td><input type="text" name="street_name" id="street_name" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="landmark">Landmark</label></td>
+							        					<td><input type="text" name="landmark" id="landmark" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="city">City</label></td>
+							        					<td><input type="text" name="city" id="city" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="state">State</label></td>
+							        					<td><input type="text" name="state" id="state" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="zip">Zip Code</label></td>
+							        					<td><input type="text" name="zip" id="zip" max=6 min=6 class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="country">Country</label></td>
+							        					<td><input type="text" name="country" id="country" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        				</table>	
+					        			</div>
+					        			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="step2" style="display: none;">
+					        				<table class="table table-responsive">			        					
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="name">Name on Card</label></td>
+							        					<td><input type="text" name="name" id="name" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="number">Card Number</label></td>
+							        					<td><input type="email" name="number" id="number" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="cvc">CVC</label></td>
+							        					<td><input type="text" name="cvc" id="cvc" class="form_control" /></td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="exp-month">Expiry Month</label></td>
+							        					<td>
+							        						<select id="exp-month" class="form_control">
+								        						<option value=""></option>
+								        						<?php for($i=1;$i<13;$i++): ?>
+								        							<option value="<?=$i;?>"><?=$i;?></option>
+								        						<?php endfor; ?>
+								        					</select>
+								        				</td>
+							        				</div>
+					        					</tr>
+					        					<tr>
+					        						<div class="form-group">
+							        					<td><label for="exp-year">Expiry Year</label></td>
+							        					<td>
+							        						<select id="exp-year" class="form_control">
+							        							<option value=""></option>
+							        							<?php $yr = date("Y");?>
+							        							<?php for($i=0;$i<11;$i++):?>
+							        								<option value="<?=$yr+$i;?>"><?=$yr+$i;?></option>
+							        							<?php endfor; ?>
+							        						</select>
+							        					</td>
+							        				</div>
+					        					</tr>
+					        				</table>
+					        			</div>
+					        	</div>
+					      	</div>
+					      	<div class="modal-footer">
+					        	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					        	<button type="button" class="btn btn-primary" onclick="check_address();" id="next_button">Next</button>
+					        	<button type="button" class="btn btn-info" onclick="back_address();" id="back_button" style="display: none;">Back</button>	
+					        	<button type="submit" class="btn btn-success" id="checkout_button" style="display: none;">Checkout</button>
+					        	</form>
+					      	</div>
 				    	</div>
 				  	</div>
 				</div>
@@ -111,5 +227,54 @@
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+
+	function back_address(){
+		jQuery('#payment-errors').html("");
+		jQuery('#step1').css("display","block");
+		jQuery('#step2').css("display","none");
+		jQuery('#next_button').css("display","inline-block");
+		jQuery('#back_button').css("display","none");
+		jQuery('#checkout_button').css("display","none");
+		jQuery('#checkoutModalLabel').html("<h4 class='modal-title text-center'>Shipping Address</h4>");
+	}
+
+	function check_address(){
+		var data = {
+			'full_name' : jQuery('#full_name').val(),
+			'email' : jQuery('#email').val(),
+			'address' : jQuery('#address').val(),
+			'street_name' : jQuery('#street_name').val(),
+			'landmark' : jQuery('#landmark').val(),
+			'city' : jQuery('#city').val(),
+			'state' : jQuery('#state').val(),
+			'zip' : jQuery('#zip').val(),
+			'country' : jQuery('#country').val(),
+		};
+		jQuery.ajax({
+			url : '/khadi/admin/parsers/check_address.php',
+			method : 'POST',
+			data : data,
+			success : function(data){
+				if(data != 'passed'){
+					jQuery('#payment-errors').html(data);					
+				}
+				if(data == 'passed'){
+					jQuery('#payment-errors').html("");
+					jQuery('#step1').css("display","none");
+					jQuery('#step2').css("display","block");
+					jQuery('#next_button').css("display","none");
+					jQuery('#back_button').css("display","inline-block");
+					jQuery('#checkout_button').css("display","inline-block");
+					jQuery('#checkoutModalLabel').html("<h4 class='modal-title text-center'>Enter your card details</h4>");
+				}
+			},
+			error : function(){
+				alert('Something went wrong');
+			},
+		});
+	}
+</script>
 
 <?php include 'includes/footer.php'; ?>
---
+++ thankYou.php
@@ -0,0 +1,1 @@
+<h2 class="text-center">Thank you!</h2>
