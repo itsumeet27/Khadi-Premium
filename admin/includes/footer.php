a3ba97c3d8390831f6d2a099e8221d53d7e28ca9
Sumeet Sharma <sksksharma0@gmail.com>
2018-11-12 16:06:40 +0530

    Add files via upload

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,25 +1,97 @@
-<script type="text/javascript">
-	function get_child_options(selected){
-		if(typeof selected === 'undefined'){
-			var selected = '';
+<!--Footer-->
+    <footer class="page-footer text-center font-small primary-color-dark darken-2 mt-4 wow fadeIn">
+        <hr class="my-4">
+
+        <!-- Social icons -->
+        <div class="pb-4">
+            <a href="https://www.facebook.com/khadipremium" target="_blank">
+                <i class="fa fa-facebook mr-3"></i>
+            </a>
+
+            <a href="https://instagram.com/khadipremium" target="_blank">
+                <i class="fa fa-instagram mr-3"></i>
+            </a>
+
+            <a href="https://www.youtube.com/" target="_blank">
+                <i class="fa fa-youtube mr-3"></i>
+            </a>
+        </div>
+        <!-- Social icons -->
+
+        <!--Copyright-->
+        <div class="footer-copyright py-3">
+            © 2018 Copyright:
+            <a href="https://localhost/khadi/index.php" target="_blank"> Khadi Premium Cosmetics </a>
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="../js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="../js/mdb.min.js"></script>
+
+    <script type="text/javascript" src="../js/datatables.js"></script>
+
+    <script type="text/javascript" src="../js/datatables.min.js"></script>
+    <script type="text/javascript">
+		function get_child_options(selected){
+			if(typeof selected === 'undefined'){
+				var selected = '';
+			}
+			var parentID = jQuery('#parent').val();
+			jQuery.ajax({
+				url: '/khadi/admin/parsers/child_categories.php',
+				type: 'POST',
+				data: {parentID : parentID, selected : selected},
+				success: function(data){
+					jQuery('#child').html(data);
+				},
+				error: function(){
+					alert("Something went wrong!");
+				},
+			});
 		}
-		var parentID = jQuery('#parent').val();
-		jQuery.ajax({
-			url: '/khadi/admin/parsers/child_categories.php',
-			type: 'POST',
-			data: {parentID : parentID, selected : selected},
-			success: function(data){
-				jQuery('#child').html(data);
-			},
-			error: function(){
-				alert("Something went wrong!");
-			},
+		jQuery('select[name="parent"]').change(function(){
+			get_child_options();
 		});
-	}
-	jQuery('select[name="parent"]').change(function(){
-		get_child_options();
-	});
-</script>
+	</script>
+    <!--Google Maps-->
+    <script src="https://maps.google.com/maps/api/js"></script>
+    <script>
+        // Regular map
+        function regular_map() {
+            var var_location = new google.maps.LatLng(40.725118, -73.997699);
+
+            var var_mapoptions = {
+                center: var_location,
+                zoom: 14
+            };
+
+            var var_map = new google.maps.Map(document.getElementById("map-container"),
+                var_mapoptions);
+
+            var var_marker = new google.maps.Marker({
+                position: var_location,
+                map: var_map,
+                title: "New York"
+            });
+        }
+
+        // Initialize maps
+        google.maps.event.addDomListener(window, 'load', regular_map);
+    </script>
+
+</body>
+
+</html>
 
 	</body>
 </html>
--- admin/includes/head.php
+++ admin/includes/head.php
@@ -1,186 +1,70 @@
-<!DOCTYPE html>
-<?php 
-  include '../core/init.php';
-?>
-  <head>
-  <title>Admin - Khadi Premium | Treasures of India</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|Montserrat" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="../css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="../css/style.min.css" rel="stylesheet">
 
-  <style type="text/css">
-    html,body{
-      overflow-x: hidden;
-    }
-    body{
-      font-family: 'Montserrat', sans-serif;
-    }
-    .container{
-      width: 90%;
-    }
-    .title{
-      font-family: 'Berkshire Swash', cursive;
-      font-size: 3em;
-      text-align: center;
-      padding: 1em;
-      color: #805a26;
-    }
-    .title-text{
-      font-family: 'Berkshire Swash', cursive;
-      font-size: 1.4em;
-      text-align: center;
-      font-weight: normal;
-    }
-    .col-lg-4, .col-md-4, .col-md-6, .col-sm-12, .col-xs-12{
-      padding: 1.5em;
-    }
-    .regime h2, .featured h2, .knowledge h2, .testimonials h2{
-      font-family: Trajan Pro;
-      font-size: 3em;
-      text-align: center;
-      padding: 1em;
-    }
-    .beauty-regime{
-      line-height: 1.5em;
-      padding: 1em;
-    }
-    .regime-shop{
-      color:#fff;
-      background-color: #805a26;
-      border-radius: 2px;
-      border: 1px solid #805a26;
-      padding: 1em 1.75em;
-    }
-    .regime-shop:hover{
-      color: #805a26;
-      background-color: #fff;     
-      transition-duration: 0.4s;
-      text-decoration: none;
-    }
-    .know-title{
-      font-family: Trajan Pro;
-      font-size: 2.5em;
-      text-align: center;
-      padding: 0.5em;
-    }
-    .know-desc{
-      text-align: justify;
-      padding: 0.5em;
-    }
-    .know-more{
-      text-align: center;
-    }
-    .know-more a{
-      color:#fff;
-      background-color: #805a26;
-      border-radius: 2px;
-      border: 1px solid #805a26;
-      padding: 0.75em 1.25em;
-    }
-    .know-more a:hover{
-      color: #805a26;
-      background-color: #fff;     
-      transition-duration: 0.4s;
-      text-decoration: none;
-    }
-    .know-head{
-      font-size: 1.5em;
-    }
-    .know{
-      width: 5em;
-      border-style: smooth;
-      border-color: #805a26;
-      border-width: 0.25em;
-      border-radius: 10px;
-    }
-    .testimonials-head{
-      font-size: 2.25em;
-      font-family: Trajan Pro;
-      text-align: center;
-    }
-    .testimonials-text{
-      font-size:  18px;
-      padding: 10em;
-      font-family: 'Berkshire Swash', cursive;
-      text-align: center;
-      line-height: 1.5em;
-    }
-    .navbar{
-      border: none;
-    }
-    .navbar-inverse{
-      background-color: #fff;
-    }
-    .navbar-inverse .navbar-nav > li > a{
-      color: #805a26;
-      font-family: Trajan Pro;
-      font-size: 1.15em;
-    }
-    .navbar-inverse .navbar-nav > li > a:hover{
-      color: #fff;
-      background-color: #805a26;
-      transition-duration: 0.4s;
-    }
-    .products, .blog{
-      box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.4);
-      padding: 2.5em;
-      border-radius: 5px;
-    }
-    .add-to-cart{
-      padding: 0.75em;
-    }
-    .prod{
-      color: #805a26;
-    }
-    .blog-title{
-      text-align: center;
-      font-family: Trajan Pro;
-      font-weight: bold;
-      padding: 0.5em;
-    }
-    .blog-desc{
-      text-align: justify;
-    }
-    .saved-image{
-      width: 200px;
-      height: 200px;
-    }
-    .del-image{
-      margin: 1em;
-    }
-    .saved-image img{
-      width: 100%;
-    }
-  </style>
+    <link href="../css/datatables.css" rel="stylesheet">
+
+    <link href="../css/datatables.min.css" rel="stylesheet">
 </head>
-<body>
-  <div>
-      <div class="row">
-        <div class="col-lg-12 col-md-12">
-          <a href="/khadi/admin/index.php"><img src="../images/download.png" class="img-responsive"></a>
-        </div>
-        <div class="col-lg-0- col-md-0">
-          <center><a href="#">My Account</a> | <a href="#">Login</a></center>
-        </div>
-    </div>
-  </div>
-  <br>
-  <!--Navigation Bar-->
-  <nav class="navbar navbar-inverse">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>                        
+
+<body class="grey lighten-3">
+        <!-- Navbar -->
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+            <div class="container-fluid">
+
+                <!-- Brand -->
+                <a class="navbar-brand waves-effect" href="" target="_blank">
+                    <strong class="blue-text">Khadi Premium</strong>
+                </a>
+
+                <!-- Collapse -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-              <ul class="nav navbar-nav">
-                  <li><a href="categories.php">Categories</a></li>
-                  <li><a href="products.php">Products</a></li>
-                  <li><a href="blog.php">Blog</a></li>
-                  <li><a href="invoice.php">Invoice</a></li>
-              </ul>
+
+                <!-- Links -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <!-- Left -->
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link waves-effect" href="products.php" target="_blank">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link waves-effect" href="categories.php" target="_blank">Categories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link waves-effect" href="blog.php" target="_blank">Blog</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right -->
+                    <ul class="navbar-nav nav-flex-icons">
+                        <li class="nav-item">
+                            <a href="https://www.facebook.com/khadipremium" class="nav-link waves-effect" target="_blank">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://instagram.com/khadipremium" class="nav-link waves-effect" target="_blank">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+
             </div>
         </nav>
+        <!-- Navbar -->
